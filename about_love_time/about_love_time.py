c52ff571e6bec90cd5f1c76ce0d7e431164f6d94
duoduo369 <duoduo3369@gmail.com>
2018-06-06 06:36:50 +0000

    修改样式

--- about_love_time/about_love_time.py
+++ about_love_time/about_love_time.py
@@ -6,6 +6,7 @@ CONFIG = [
     (u'第一次相遇', '2018-01-29'),
     (u'第一次深聊', '2018-05-11'),
     (u'确认情侣关系', '2018-05-20'),
+    (u'', '2018-06-02'),
 ]
 
 def main(config=CONFIG):
@@ -14,7 +15,7 @@ def main(config=CONFIG):
     for each in config:
         key, date = each
         days = (now - arrow.get(date)).days
-        print u'从我们{}至今 已经 {} 天了'.format(key, days)
+        print u'从我们{}至今已经 {} 天了'.format(key, days)
 
 
 if __name__ == '__main__':
